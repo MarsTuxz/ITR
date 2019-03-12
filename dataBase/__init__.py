3ecdef5093fc9bbe4a9b143da28a130d06bfd1bb
tuxz <1029745198@qq.com>
2019-03-12 10:38:45 +0800

    爬虫开发
    Signed-off-by: mars <18270880901@163.com>

---
+++ dataBase/__init__.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python
+#-*- coding:utf-8 _*-
+"""
+@author:mars
+@file: __init__.py.py
+@time: 2018/11/20
+"""
+
+
+
+# insert_sql = 'INSERT INTO poem_demos (demo1, demo2, title, link) VALUES ("{}", "{}", {}, {});'
+# s = insert_sql.format('23', 'er', 're', 'ty')
+# print(s)
