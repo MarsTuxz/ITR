eaf6345020ae9ed3f594a45c3420973dc906e63d
yuli <1029745198@qq.com>
2019-03-27 10:44:00 +0800

    common:公共代码 dataBase:数据库链接 Image_reco:图像处理 milestone：交付及里程碑文档 resources：公共资源 spider：爬虫部分 Test：测试 tool：工具

--- Test/mars_test.py
+++ Test/mars_test.py
@@ -5,9 +5,6 @@
 @file: mars_test.py
 @time: 2019/03/07
 """
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 import pytesseract
 from PIL import Image
 text = pytesseract.image_to_string(Image.open('test1.png'))
