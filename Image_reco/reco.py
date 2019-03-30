64787701d73d9060d0e63c15dfaf03dfc8ba6704
tuxz <1029745198@qq.com>
2019-03-30 20:06:57 +0800

    Mars & Yu_图片处理及识别2.0

--- Image_reco/reco.py
+++ Image_reco/reco.py
@@ -1,7 +1,18 @@
 from PIL import Image
 import pytesseract
-# ttt
+import scipy.misc as misc
+
 class Image_rec(object):
-    def feedbackWord(self,address):
-        text = pytesseract.image_to_string(Image.open(address), lang='chi_sim').replace(" ", "")
-        return text
+        def size_change(self, address):
+                img = misc.imread(address)
+                (w, h, n) = img.shape
+                w = int(w * 0.3)
+                h = int(h * 0.3)
+                img = misc.imresize(img, (w, h, 3), interp='bilinear')
+                misc.imsave(address, img)
+                print("size_change function end")
+
+        def feedbackWord(self,address):
+                text = pytesseract.image_to_string(Image.open(address), lang='chi_sim').replace(" ", "")
+                print("feedbackWord function end")
+                return text
